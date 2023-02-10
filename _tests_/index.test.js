7d9410101d39aa0ce794115bdb454cf28116d5c8
alylmeier <alylmeier@gmail.com>
2023-02-10 15:48:43 -0500

    all tests passing

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -24,16 +24,24 @@ describe("Employee", () => {
 describe("Engineer", () => {
   // A test is created to check that modulus does in fact return the remainder of the quotient of the two numbers.
   describe("input", () => {
-    it("should create an object with a name, id, email, and misc", () => {
+    it("should create an object with a title, name, id, email, and misc", () => {
+      const newTitle = "";
       const newName = "";
       const newId = "";
       const newEmail = "";
       const newMisc = "";
-      const engineer = new Engineer(newName, newId, newEmail, newMisc);
+      const engineer = new Engineer(
+        newTitle,
+        newName,
+        newId,
+        newEmail,
+        newMisc
+      );
+      expect(engineer.title).toEqual(newTitle);
       expect(engineer.name).toEqual(newName);
       expect(engineer.id).toEqual(newId);
       expect(engineer.email).toEqual(newEmail);
-      //expect(engineer.newMisc).toEqual("Github Username: " + newMisc);
+      expect(engineer.misc).toEqual("Github Username: " + newMisc);
     });
   });
 });
@@ -41,30 +49,37 @@ describe("Engineer", () => {
 describe("Manager", () => {
   // A test is created to check that modulus does in fact return the remainder of the quotient of the two numbers.
   describe("input", () => {
-    it("should create an object with a name, id, email, and office number", () => {
+    it("should create an object with a title name, id, email, and misc", () => {
+      const newTitle = "";
       const newName = "";
       const newId = "";
       const newEmail = "";
-      const manager = new Manager(newName, newId, newEmail);
-
+      const newMisc = "";
+      const manager = new Manager(newTitle, newName, newId, newEmail, newMisc);
+      expect(manager.title).toEqual(newTitle);
       expect(manager.name).toEqual(newName);
       expect(manager.id).toEqual(newId);
-      //expect(manager.email).toEqual(newEmail);
+      expect(manager.email).toEqual(newEmail);
+      expect(manager.misc).toEqual("Office number: " + newMisc);
     });
   });
 });
 describe("Intern", () => {
   // A test is created to check that modulus does in fact return the remainder of the quotient of the two numbers.
   describe("input", () => {
-    it("should create an object with a name, id, email, and school", () => {
+    it("should create an object with a title, name, id, email, and misc", () => {
+      const newTitle = "";
       const newName = "";
       const newId = "";
       const newEmail = "";
-      const intern = new Intern(newName, newId, newEmail);
+      const newMisc = "";
+      const intern = new Intern(newTitle, newName, newId, newEmail, newMisc);
 
+      expect(intern.title).toEqual(newTitle);
       expect(intern.name).toEqual(newName);
       expect(intern.id).toEqual(newId);
-      //expect(intern.email).toEqual(newEmail);
+      expect(intern.email).toEqual(newEmail);
+      expect(intern.misc).toEqual("Attends: " + newMisc);
     });
   });
 });
